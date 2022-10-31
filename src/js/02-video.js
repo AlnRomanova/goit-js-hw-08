120a7cdbcdebe576a87e0836b874d99893f6bb4e
Alina Romanova <alinakhraban@gmail.com>
2022-10-31 22:18:39 +0200

    Add edits

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,24 +3,24 @@ import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+  const player = new Player(iframe);
 
 
-    player.on('timeupdate', throttle(function(currentTime) {
-      localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+player.on('timeupdate', throttle(function(currentTime) {
+    localStorage.setItem('videoplayer-current-time', currentTime.seconds);
   }, 1000));
 
-  const savedTime = localStorage.getItem('videoplayer-current-time');
+const savedTime = localStorage.getItem('videoplayer-current-time');
 
-  player.setCurrentTime(savedTime).then(function(seconds) {
+player.setCurrentTime(savedTime).then(function(seconds) {
   }).catch(function(error) {
-      switch (error.name) {
-          case 'RangeError':
-              break;
+    switch (error.name) {
+    case 'RangeError':
+    break;
 
-          default:
-              break;
-      }
+    default:
+    break;
+    }
   });
 
 
