84e111fda5699382760a363cdc3a7ef36128667c
AlnRomanova <111745365+AlnRomanova@users.noreply.github.com>
2022-10-31 23:19:03 +0000

    Deploying to gh-pages from @ AlnRomanova/goit-js-hw-08@1d4046627bfedaac471895840fda871034119c28 🚀

--- 03-feedback.e82fc442.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form"),t=document.querySelector("input");function n(){t.textContent=localStorage.getItem("feedback-form-state")||""}n(),e.addEventListener("input",(function(t){t.preventDefault(),localStorage.setItem("feedback-form-state",e.elements.email.value,e.elements.message.value),n(),e.reset()}));
-//# sourceMappingURL=03-feedback.e82fc442.js.map
